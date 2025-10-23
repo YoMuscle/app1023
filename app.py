f6558aeb2b388164904ece6caadd9ba775df30bf
YoMuscle <ntustcs110@gmail.com>
2025-10-23 14:05:08 +0800

    update library

--- app.py
+++ app.py
@@ -1,5 +1,6 @@
 from flask import Flask, render_template, request, jsonify, redirect, url_for
 from flask_sqlalchemy import SQLAlchemy
+import os
 
 app = Flask(__name__)
 # app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///blog.db'
